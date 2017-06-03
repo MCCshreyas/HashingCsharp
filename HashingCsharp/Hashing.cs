5f9034849b6e7854e64be018c84817ae36a863bb
Shreyas jejurkar <shreyasjejurkar123@live.com>
2017-06-04 02:15:29 +0530

    Added project files

---
+++ HashingCsharp/Hashing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hashing
+{
+    class Hashing
+    {
+        double[] phoneNumbers = new double[10];
+        int Id = 0, hashValue = 0;
+        
+        public void AddNumbers()
+        {
+            Console.WriteLine("Enter the Id of the student");
+            Id = int.Parse(Console.ReadLine());
+            hashValue = Id % 10;
+            Console.WriteLine("The hash value generated is " + hashValue);
+            Console.WriteLine("Now enter the phone number of the student");
+            phoneNumbers[hashValue] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Phone number added sucessfully");
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < phoneNumbers.Length; i++)
+            {
+                Console.WriteLine("PhoneNumbers[{0}] = {1} ", i, phoneNumbers[i]); 
+            }
+        }
+
+
+    }
+}
