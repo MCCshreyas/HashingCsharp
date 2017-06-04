6ab07ed4e387ca8f96b83d12eb7d252ee823ecf9
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2017-06-04 15:34:54 +0530

    Added comments.
     Added comments to the code for better understanding.

--- HashingCsharp/Hashing.cs
+++ HashingCsharp/Hashing.cs
@@ -7,20 +7,30 @@ namespace Hashing
 {
     class Hashing
     {
-        double[] phoneNumbers = new double[10];
+        /*
+          declaration of double array where we are going to save phone numbers. 
+          Hash table size is 10.
+        */
+        
+        double[] phoneNumbers = new double[10];              
         int Id = 0, hashValue = 0;
         
         public void AddNumbers()
         {
             Console.WriteLine("Enter the Id of the student");
             Id = int.Parse(Console.ReadLine());
-            hashValue = Id % 10;
+            hashValue = Id % 10;                                             //calculate hash value from hash function X mod 10.
             Console.WriteLine("The hash value generated is " + hashValue);
             Console.WriteLine("Now enter the phone number of the student");
-            phoneNumbers[hashValue] = double.Parse(Console.ReadLine());
+            phoneNumbers[hashValue] = double.Parse(Console.ReadLine());      //save the phone number to hash index.
             Console.WriteLine("Phone number added sucessfully");
         }
 
+        
+        /*
+            following method will display the content in the array
+        */
+        
         public void Display()
         {
             for (int i = 0; i < phoneNumbers.Length; i++)
